36d7ae3297a9ee9f9ae3dfbd0b79eebd8e36dfec
Dhanuvarsha S S <dhanuvarsha49@gmail.com>
2025-03-15 18:05:08 +0000

    task4commits

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "For task 4  \n";
+  cout << "For task 4 nowww  \n";
   return 0;
 }
